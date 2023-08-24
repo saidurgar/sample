645c7874900895c26206962f041255cd3645c41f
saidurgar <raminenisaidurga@gmail.com>
2023-08-24 18:09:38 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenceItem() {
+    return (
+        <div>
+            <h2>Expence Item</h2>
+            <div>Food Rs 10</div>
+            <div>Petrol Rs 100</div>
+            <div>Movie Rs 200</div>
+        </div>
+    );
+}
+export default ExpenceItem;
